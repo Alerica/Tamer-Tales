be1cb723fef3c1bc39b1306e1b07c2a1cdeb95fd
Alexander Ricky <146740660+Alerica@users.noreply.github.com>
2024-05-24 12:06:30 +0700

    Add files via upload

---
+++ Tamer_Tales_Display.h
@@ -0,0 +1,21 @@
+int menuPokemon();
+/*
+    This will return int between 1 - 6
+*/
+void menuPokemonDisplay(int current);
+void menuPokemonNavigation(int nav);
+
+int menuViewPokemon();
+/*
+    This will return int between 1 - 4
+*/
+void menuViewPokemonNavigation(int nav);
+void menuViewPokemonDisplay(int current);
+
+
+int viewPokemonGeneration();
+/*
+    This will return int between 1-9
+*/
+void viewPokemonGenerationDisplay(int current);
+void viewPokemonGenerationNavigation(int nav);
