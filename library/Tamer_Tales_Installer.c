d299bb3879bbab9c3ba55f210beef36be721d682
Alexander Ricky <146740660+Alerica@users.noreply.github.com>
2024-06-18 15:10:59 +0700

    Delete library directory

--- library/Tamer_Tales_Installer.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    system("gcc -o Tamer_Tales Tamer_Tales_Main.c Tamer_Tales_Graph.c  Tamer_Tales_FileProcessing.c Tamer_Tales_Display.c");
-    system("Tamer_Tales.exe");
-}
