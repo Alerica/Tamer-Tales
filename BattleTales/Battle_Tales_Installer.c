26f9f1c60ad7116836548d04e5890528e614299e
Rafly Adha Prathama <144401868+ShiroeWT@users.noreply.github.com>
2024-06-19 07:31:01 +0700

    Update Battle_Tales_Installer.c (-lwinmm di ujung installer battle_Tales)

--- BattleTales/Battle_Tales_Installer.c
+++ BattleTales/Battle_Tales_Installer.c
@@ -6,7 +6,7 @@ int main() {
 
     system("gcc -o Battle_Tales_MapPalleteTown Battle_Tales_MapPalleteTown.c Battle_Tales_Display.c Battle_Tales_UI.c Battle_Tales_FileProcessing.c Battle_Tales_Performance.c  Battle_Tales_PokemonStruct.c Battle_tales_Inventory.c Battle_Tales_Map.c -lwinmm");
     system("gcc -o Battle_Tales Battle_Tales_Main.c Battle_Tales_Display.c Battle_Tales_UI.c Battle_Tales_FileProcessing.c Battle_Tales_Performance.c  Battle_tales_Inventory.c Battle_Tales_PokemonStruct.c Battle_Tales_Map.c -lwinmm");
-    system("gcc -o Battle_Tales_Battle Battle_Tales_PokemonStruct2.c Battle_Tales_UI.c");
+    system("gcc -o Battle_Tales_Battle Battle_Tales_PokemonStruct2.c Battle_Tales_UI.c -lwinmm");
 }
 
 
